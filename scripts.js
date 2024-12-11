af043a42d6a0efd8dbd715d9da55710665bcb467
TimesCode <timescodeinfo@gmail.com>
2024-12-11 18:27:48 +0530

    Portfolio Update 1

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["UI/UX Designer", "Web Developer", "Freelancer", "Mobile App Designer", "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 800,
+    loop: true
+});
